a1168c6b543f4de397de06d0be8163eb71ab2ff9
tarekabdelsalam <tarekrs@gmail.com>
2015-08-13 16:17:47 -0700

    Update image.php

--- image.php
+++ image.php
@@ -1,9 +1,9 @@
 <?php
 // Create a 55x30 image
 $im = imagecreatetruecolor(200, 200);
-$white = imagecolorallocate($im, 255, 255, 255);
+//$white = imagecolorallocate($im, 255, 255, 255);
 $blue = imagecolorallocate($im, 0, 0, 255);
-//$green = imagecolorallocate($im, 0, 255, 0);
+$green = imagecolorallocate($im, 0, 255, 0);
 
 // Draw a white rectangle
 imagefilledrectangle($im, 0, 0, 199, 199, $blue);
